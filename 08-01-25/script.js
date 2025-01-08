1b965ae74bfa4ff1a137abc1c4a52a7f61117e08
Anaïs <diez.anais@gmail.com>
2025-01-08 16:12:24 +0100

    Add files via upload

--- 08-01-25/script.js
+++ 08-01-25/script.js
@@ -91,9 +91,9 @@ email.addEventListener('keyup', emailValide ); // mettre avant la fonction ?
 
 function emailValide() {
     const emailValue = email.value;
-    const emailPattern = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/;
+    const regexMail = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/;
 
-    if (emailPattern.test(emailValue)) {
+    if (regexMail.test(emailValue)) {
         email.style.background = "green";
     } else {
         email.style.background = "red";
@@ -102,19 +102,70 @@ function emailValide() {
 
 
 // pas fini ^_^'
-const motDePasse = document.querySelector("#mot");
-const confirm = document.querySelector("#confirm");
+// const motDePasse = document.querySelector("#mot");
+// const confirm = document.querySelector("#confirm");
 
-function MDPValide() {
-    const motDePasseValue = motDePasse.value;
+// function MDPValide() {
+//     const motDePasseValue = motDePasse.value;
+//     const charDecimal = /\d/;
+//     const charSpecial = /[$&@!]/;
+
+//     if (charDecimal.match(motDePasseValue)) {
+//         confirm.innerText = "mot de passe valide";
+//     } else {
+//         confirm.innerText = "mot de pase non valide";
+//     }
+// };
+
+// motDePasse.addEventListener('keyup', MDPValide );
+
+
+//Solution formateur
+const loginInput = document.querySelector('#exampleInputEmail1');
+const passwordInput = document.querySelector('#exampleInputPassword1');
+const userMessage = document.querySelector('#userMessage');
+
+loginInput.addEventListener('keyup',()=>{ 
+ // Ajoute un écouteur d'événements pour détecter les frappes de touches sur le champ de saisie de l'email
+ const regexMail = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/; 
+ // Définit une expression régulière pour valider le format de l'email
+ if(regexMail.test(loginInput.value)){ 
+   loginInput.style.backgroundColor = 'lightgreen'; 
+   // Change la couleur de fond en rouge si l'email est invalide
+ }
+ else{  // Vérifie si la valeur saisie ne correspond pas à l'expression régulière
+   loginInput.style.backgroundColor = 'red'; // Change la couleur de fond en vert
+ }
+});
+
+passwordInput.addEventListener('keyup',()=>{
     const charDecimal = /\d/;
     const charSpecial = /[$&@!]/;
-
-    if (charDecimal.match(motDePasseValue)) {
-        confirm.innerText = "mot de passe valide";
-    } else {
-        confirm.innerText = "mot de pase non valide";
+    let errorMessage ='';
+    // Si MDP <6 alors dans errormessage on cumule une <li></li> (trop court)
+    // Sinon Si MDP >8 alors dans errormessage on cumule une <li></li> (trop long)
+    // Si la value du passWord ne match pas la regex decimale alors on cumule une <li></li> (faut un chiffre)
+    // etc ... meme système pour les autres cas 
+    // On peut aussi faire un truc genre si erroMessage est vide alors c'est tout bon 
+    if(passwordInput.value.length<6){
+      errorMessage+='<li>Le Mot de passe trop COURT</li>'
     }
-};
-
-motDePasse.addEventListener('keyup', MDPValide );
+    else if(passwordInput.value.length>8){
+      errorMessage+='<li>Le Mot de passe trop LONG</li>'
+    }
+  
+    if(!passwordInput.value.match(charDecimal)){
+      errorMessage+='<li>Le Mot de passe doit contenir 1 chiffre</li>'
+    }
+    if(!passwordInput.value.match(charSpecial)){
+      errorMessage+='<li>Le Mot de passe doit contenir 1 caractère spécial (@,&,!,$,)</li>'
+    }
+    if(errorMessage!==''){
+      userMessage.innerHTML = `le mot de passe est : <ul>${errorMessage}</ul>`;
+      userMessage.style.border ='5px solid red'
+    }
+    else{
+      userMessage.innerHTML = 'Le mot de passe est valide ! 👍';
+      userMessage.style.border ='5px solid green'
+    }
+});
